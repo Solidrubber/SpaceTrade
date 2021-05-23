6d6e63fcdac6a11e20e7f2bb32ecca386287bea3
Solidrubber <derwitt@gmail.com>
2021-05-23 17:09:15 +0200

    Erweiterung des Models/Schemas

--- app/routers/ship.py
+++ app/routers/ship.py
@@ -1,8 +1,22 @@
-from fastapi import APIRouter
+from fastapi import Depends, APIRouter
+from typing import Optional
+from .. import schemas, models
+from ..database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 router = APIRouter()
 
 fake_items_db = {"Schiff": {"name": "Kunoabgsoffen"}, "SchiffII": {"name": "Blubbe"}}
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @router.get("/ship")
 async def read_items():
